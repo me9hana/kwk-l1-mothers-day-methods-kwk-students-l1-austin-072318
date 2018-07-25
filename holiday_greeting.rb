2d857690b4cd6863b182b966e641fe1b3d4c837e
me9hana <meghanagoli@gmail.com>
2018-07-25 04:34:57 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,7 +1,7 @@
 def holiday_greeting(name1 = "Mom", event = "Mother's Day", name2 = "Your Favorite Child")
   "Happy #{event}, #{name1}! - From #{name2}"
 end
-
+puts holiday_greeting("Beyonce", "Jay-Z", "Fourth of July")
 
 
 
